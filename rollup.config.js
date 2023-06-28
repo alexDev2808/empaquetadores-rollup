f97115ab121dbf9f1969fb47e2fd4518190d166c
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-28 11:16:52 -0600

    Configuracion de plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import html from "@rollup/plugin-html"
+import copy from "rollup-plugin-copy"
+import css from "rollup-plugin-css-only"
+import image from "@rollup/plugin-image"
+
 export default {
     input: "src/index.js",
     output: {
@@ -5,7 +10,19 @@ export default {
         format: "es",
         sourcemap: true,
     },
-    plugins: {
-        
-    }
+    plugins: [
+        copy({
+            targets: [
+                {
+                    src: "src/assets/**/*",
+                    dest: "dist/asset"
+                }
+            ]
+        }),
+        css({
+            output: 'bundle.css'
+        }),
+        html(),
+        image(),
+    ]
 }
