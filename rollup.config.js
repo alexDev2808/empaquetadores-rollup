a8854559315329eed96d54568b3337b538223ef4
J. Alexis TH <colores.2808.8000@gmail.com>
2023-06-28 11:10:30 -0600

    Configuracion inicial

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+export default {
+    input: "src/index.js",
+    output: {
+        file: "dist/bundle.js",
+        format: "es",
+        sourcemap: true,
+    },
+    plugins: {
+        
+    }
+}
